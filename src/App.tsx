4fc5c256621aa69ae23049cd8c3a72ce4435784c
“JunSecondStart” <junsama.silva.dodoria@gmail.com>
2024-10-28 16:18:20 +0900

    make, about tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Pages } from './views/pages';
-import "./App.css"
 
 export const App = () => {
   return (
     <>
-      <head>
-        <link href="./App.css" rel="stylesheet"/>
-      </head>
       <div>
         <Pages />
       </div>
