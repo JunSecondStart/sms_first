b354913af6da09aff0286335d1bb9f51b4ed1b04
“JunSecondStart” <junsama.silva.dodoria@gmail.com>
2024-11-02 10:26:46 +0900

    id,increment is OK.content, inputed text is refered from textarea.

--- src/views/components/SendSMSCreated.tsx
+++ src/views/components/SendSMSCreated.tsx
@@ -1,30 +1,51 @@
-import React from "react";
-import { Home } from "./Home";
+import React, { useState } from "react";
 import { useSms } from "../../context";
-import { SmsState } from "../../types/index.d";
+import { SmsState,SmsContext } from "../../types/index.d";
 
 export const SendSMSCreated: React.FC = () => {
   const sms = useSms();
+  const [textIdIncremented,settextIdIncremented]=useState(1);
+  const [textTitle,setTextTitle] = useState("title,default");
+  const [textContent,setTextContent] = useState("content,default");
   const smsData:SmsState={
-    id: 2,
-    phone_number_id: 2,
-    user_id: 2,
-    title: "title",
-    content_text: "content_text",
+    id: textIdIncremented,
+    phone_number_id: 1,
+    user_id: 1,
+    title: textTitle,
+    content_text: textContent,
     created_at: "created_at",
     updated_at: "updated_at",
     sent_at: "sent_at",
   };
 
+  const handleChangedText=(e:React.ChangeEvent<HTMLTextAreaElement>)=>{
+    setTextTitle(e.target.value);
+  };
+
+  const handleChangedContent=(e:React.ChangeEvent<HTMLTextAreaElement>)=>{
+    setTextContent(e.target.value);
+  };
+  
   const createSMS = () => {
-    // sms.push();
-    sms.smsSentByUser(smsData);
-  }
+    settextIdIncremented(textIdIncremented+1);
+    sms.smsSentByUser(smsData);    
+  };
   return (
     <>
       <body>
         sendSMSCreated
         <div className="bg-sky-200" onClick={()=>createSMS()}>Push!!, here.</div>
+          <textarea
+            value={textTitle}
+            onChange={handleChangedText}
+            placeholder="title"
+          />
+          <textarea
+          value={textContent}
+          onChange={handleChangedContent}
+          placeholder="content"
+        />
+          <button onClick={()=>createSMS()}>create</button>
       </body>
     </>
   );
